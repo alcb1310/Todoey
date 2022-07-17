de2bd3fdaaaba1d07f3ca8de9bb4d00be1405e5e
Andrés Court <andres@andrescourt.com>
2022-07-16 23:36:02 -0500

    Created table view and made it possible to populate with an array, also created Constants file to save there all constants that will be needed in the project

---
+++ Todoey/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Todoey
+//
+//  Created by Andres Court on 16/7/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct K{
+    static let cellIdentifier = "ToDoItemCell"
+}
